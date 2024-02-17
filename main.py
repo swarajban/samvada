e4c846824dc0d234e7d3485fd40a79bd26d67110
swa <swaraj@semprehealth.com>
2024-02-17 10:23:50 -0800

    scaffolding for twilio audio stream WS parsing

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Form, WebSocket
 from fastapi.responses import Response
-from twilio.twiml.voice_response import VoiceResponse
+from twilio.twiml.voice_response import Connect, VoiceResponse, Stream, Parameter
+
+BASE_HOST = 'samvada.ngrok.app'
 
 app = FastAPI()
 
@@ -10,9 +14,41 @@ async def read_root():
     return {"health": "ok"}
 
 
-@app.post("/incoming_call")
-async def handle_incoming_call():
+async def _say_something_twiml_response() -> VoiceResponse:
     response = VoiceResponse()
-    response.say("You are a bagel.")
+    n = 3
+    h = ". ".join(["airplane"] * n)
+    response.say(f"Hi Amaya? - what are some of your other favorite movies now? ", voice='woman')
     response.hangup()
+    return response
+
+
+@app.post("/incoming_call")
+async def handle_incoming_call(CallSid: Annotated[str, Form()]):
+    response = VoiceResponse()
+    connect = Connect()
+    ws_url = f"wss://{BASE_HOST}/ws"
+    stream = Stream(name=CallSid, url=ws_url)
+    stream.append(Parameter(name='test_name', value='test_value'))
+    connect.append(stream)
+    response.append(connect)
+    # response = await _say_something_twiml_response()
+    print(CallSid)
     return Response(content=str(response), media_type="application/xml")
+
+
+@app.websocket("/ws")
+async def websocket_handler(websocket: WebSocket):
+    await websocket.accept()
+    print('ws connection connected')
+    while True:
+        data = await websocket.receive_json()
+        event = data.get('event')
+        if event in ('connected', 'start'):
+            print(f'Event: {event}')
+        elif event == 'media':
+            print(f"Received media: {data}")
+        elif event == 'stop':
+            print('ws connection stopped')
+            break
+    print('ws connection disconnected')
